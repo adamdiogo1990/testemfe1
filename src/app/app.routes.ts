89632127a632ab974f964c5b2baec08579cbeee7
adamsouzaloopt <adam.souza@loopt.com.br>
2024-04-12 16:01:02 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+// Add this import:
+import { loadRemoteModule } from '@angular-architects/native-federation';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'teste2',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'flights',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: 'teste',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
+];
